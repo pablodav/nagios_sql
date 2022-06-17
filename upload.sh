b02f29bb45b8eb9b7da36da368c0ced9a55fbdd0
Pablo Estigarribia <pablodav@gmail.com>
2022-06-17 15:48:06 -0300

    fix upload to pypi requirements command

--- upload.sh
+++ upload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # requires pip3 install wheel
 # for future simplicity evaluate flit
-sudo python3 -m pip install wheel twine 
+python3 -m pip install --user wheel twine 
 rm -r dist/*
 python3 setup.py sdist | grep defaults
 python3 setup.py bdist_wheel | grep defaults
